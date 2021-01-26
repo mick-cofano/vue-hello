42e04a7f4077cd52b9302099af480f2d15b215a1
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-27 00:15:08 +0100

    Creato messaggio in vueJS e stampato in un h1 a schermo

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Ciao, questo è un messaggio scritto in VueJS e stampato in un H1 in HTML',
+    image: ''
+  }
+});
+Vue.config.devtools = true;
